8afde9f8c146ddcdfa3f2ce0e4010e00bf11a8a7
arch7tect <arch7tect@gmail.com>
2020-09-14 14:20:20 +0300

    quick start

--- bd-runtime/datagram/entrypoint.sh
+++ bd-runtime/datagram/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/java -Xms2g -Xmx6g -Dfile.encoding=UTF-8 -Dldap.enabled=false -Dmaven.home=/usr/share/java/maven-3 \
+/usr/bin/java -Xms2g -Xmx6g -Dfile.encoding=UTF-8 -Dldap.enabled=false -Dldap.always_admin=true -Dmaven.home=/usr/share/java/maven-3 \
   -Ddeploy.dir=/opt/datagram/mspace -Dteneo.url=jdbc:postgresql://hivemetastore:5432/teneo \
   -Dteneo.user=postgres -Dteneo.password=new_password -Dserver.port=8089 \
   -jar /opt/datagram/mserver-2.0-SNAPSHOT.jar
