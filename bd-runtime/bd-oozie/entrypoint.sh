b9a100c874722ec4242570647fb0b84b63b2ce89
arch7tect <arch7tect@gmail.com>
2020-10-08 18:26:58 +0300

    oozie hadoop version 3.2.0

--- bd-runtime/bd-oozie/entrypoint.sh
+++ bd-runtime/bd-oozie/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-$OOZIE_HOME/bin/oozie-setup.sh sharelib create -fs ? -locallib $OOZIE_HOME/sharelib
+hdfs dfs -test -e /user/root/share/lib
+if [ $? -ne 0 ]
+then
+  $OOZIE_HOME/bin/oozie-setup.sh sharelib create -fs hdfs://master:9000 -locallib $OOZIE_HOME/sharelib
+fi
 $OOZIE_HOME/bin/oozie-setup.sh db create -run
 $OOZIE_HOME/bin/oozied.sh start
 /scripts/parallel_commands.sh "/scripts/watchdir $OOZIE_HOME/logs"
