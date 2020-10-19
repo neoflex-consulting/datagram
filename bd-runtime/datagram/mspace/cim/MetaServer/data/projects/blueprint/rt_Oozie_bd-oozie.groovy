a20ef9569e827d9db57b0dfe63bb9bced2b17bdf
arch7tect <orlov@neoflex.ru>
2020-10-20 00:12:53 +0300

    blueprint project

---
+++ bd-runtime/datagram/mspace/cim/MetaServer/data/projects/blueprint/rt_Oozie_bd-oozie.groovy
@@ -0,0 +1,5 @@
+import ru.neoflex.meta.utils.Context
+def session = Context.current.txSession
+def entity = session.createQuery("from rt.Oozie where name = :name").setParameter("name", "bd-oozie").uniqueResult() 
+entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "blueprint").uniqueResult()
+Context.current.commit()
