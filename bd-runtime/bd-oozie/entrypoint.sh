8edcb651f8b28c4f1e71a62353d1c8d6a3060ac8
arch7tect <orlov@neoflex.ru>
2020-10-20 10:06:14 +0300

    move jar hell cleanup to entrypoint

--- bd-runtime/bd-oozie/entrypoint.sh
+++ bd-runtime/bd-oozie/entrypoint.sh
@@ -8,6 +8,8 @@ then
   $OOZIE_HOME/bin/oozie-setup.sh sharelib create -fs hdfs://master:9000 -locallib $OOZIE_HOME/sharelib
 fi
 $OOZIE_HOME/bin/oozie-setup.sh db create -run
+#Jar hell
+rm -rf $OOZIE_HOME/lib/jackson-*2.6.5*
 $OOZIE_HOME/bin/oozied.sh start
 /scripts/parallel_commands.sh "/scripts/watchdir $OOZIE_HOME/logs"
 $OOZIE_HOME/bin/oozied.sh stop
