1fb1634a11994b872c991be208a0107f61a697bd
hedaira <mezzo666@mail.ru>
2020-12-21 18:30:31 +0300

    changed jar name

--- bd-runtime/datagram/entrypoint.sh
+++ bd-runtime/datagram/entrypoint.sh
@@ -3,4 +3,4 @@ cd /opt/datagram
 /usr/bin/java -Xms2g -Xmx6g -Dfile.encoding=UTF-8 -Dldap.enabled=false -Dldap.always_admin=true -Dmaven.home=/usr/share/java/maven-3 \
   -Ddeploy.dir=/opt/datagram/mspace -Dteneo.url=jdbc:postgresql://hivemetastore:5432/teneo \
   -Dteneo.user=postgres -Dteneo.password=new_password -Dserver.port=8089 \
-  -jar /opt/datagram/mserver-2.0-SNAPSHOT.jar
+  -jar /opt/datagram/mserver-spark3-2.0-SNAPSHOT.jar
