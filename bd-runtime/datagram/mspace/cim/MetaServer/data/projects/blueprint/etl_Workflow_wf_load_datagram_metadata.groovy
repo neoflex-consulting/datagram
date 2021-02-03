adaacd92bf6d0c28d4683d771d62c6257b49cbcf
arch7tect <arch7tect@gmail.com>
2021-02-03 14:35:28 +0300

    oozie jar hell

--- bd-runtime/datagram/mspace/cim/MetaServer/data/projects/blueprint/etl_Workflow_wf_load_datagram_metadata.groovy
+++
@@ -1,6 +0,0 @@
-import ru.neoflex.meta.utils.Context
-def session = Context.current.txSession
-def entity = session.createQuery("from etl.Workflow where name = :name").setParameter("name", "wf_load_datagram_metadata").uniqueResult() 
-entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "blueprint").uniqueResult()
-entity.nodes.find {it.name == "DGTransformation"}.transformation = session.createQuery("from etl.Transformation where name = :name").setParameter("name", "tr_load_datagram_transformations").uniqueResult()
-Context.current.commit()
