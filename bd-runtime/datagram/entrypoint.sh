4f66ffaf49aacfeba9ecebf2f34a958893af55bb
arch7tect <arch7tect@gmail.com>
2021-02-19 15:47:32 +0300

    move datagram run parameters to env.properties and application.properties

--- bd-runtime/datagram/entrypoint.sh
+++ bd-runtime/datagram/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /opt/datagram
-/usr/bin/java -Xms2g -Xmx6g -Dfile.encoding=UTF-8 -Dldap.enabled=false -Dldap.always_admin=true -Dmaven.home=/usr/share/java/maven-3 \
-  -Ddeploy.dir=/opt/datagram/mspace -Dteneo.url=jdbc:postgresql://hivemetastore:5432/teneo \
-  -Dteneo.user=postgres -Dteneo.password=new_password -Dserver.port=8089 \
-  -jar /opt/datagram/mserver-spark3-2.0.0-SNAPSHOT.jar
+/usr/bin/java -Xmx${MEM_MAX} -Dteneo.url=${TENEO_URL} -Dteneo.user=${TENEO_USER} -Dteneo.password=${TENEO_PASSWORD} \
+  -Dfile.encoding=UTF-8 -jar mserver.jar
