0eca5c58fc67f6bf62aa05ed3632bd56bf6b36e1
asuvorova <mezzo666@mail.ru>
2021-03-19 10:48:08 +0300

    changed transformation in BluePrint

--- bd-runtime/datagram/mspace/cim/MetaServer/data/projects/blueprint/etl_Transformation_tr_aw_salary_analysis_step1.groovy
+++ bd-runtime/datagram/mspace/cim/MetaServer/data/projects/blueprint/etl_Transformation_tr_aw_salary_analysis_step1.groovy
@@ -6,8 +6,8 @@ entity.mavenDependencies.clear()
 entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "blueprint").uniqueResult()
 entity.targets.find {it.name == "in_HDFS_1"}.context = null
 entity.targets.find {it.name == "in_HDFS_2"}.context = null
-entity.sources.find {it.name == "EmployeePayHistory_2"}.context = session.createQuery("from etl.JdbcContext where name = :name").setParameter("name", "awHR").uniqueResult()
+entity.sources.find {it.name == "EmployeePayHistory_2_2"}.context = session.createQuery("from etl.JdbcContext where name = :name").setParameter("name", "awHR").uniqueResult()
 entity.sources.find {it.name == "EmployeePayHistory_1_2"}.context = session.createQuery("from etl.JdbcContext where name = :name").setParameter("name", "awHR").uniqueResult()
 entity.sources.find {it.name == "EmployeePayHistory_1"}.context = session.createQuery("from etl.JdbcContext where name = :name").setParameter("name", "awHR").uniqueResult()
-entity.sources.find {it.name == "EmployeePayHistory_2_2"}.context = session.createQuery("from etl.JdbcContext where name = :name").setParameter("name", "awHR").uniqueResult()
+entity.sources.find {it.name == "EmployeePayHistory_2"}.context = session.createQuery("from etl.JdbcContext where name = :name").setParameter("name", "awHR").uniqueResult()
 Context.current.commit()
