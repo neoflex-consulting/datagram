deca51b1b27b1870ca94fb846a6826773db2882b
hedaira <mezzo666@mail.ru>
2021-04-05 14:43:34 +0300

    add in LabWOrks

--- bd-runtime/datagram/mspace/cim/MetaServer/data/projects/labWorks/etl_Transformation_tr_salary_step2.groovy
+++ bd-runtime/datagram/mspace/cim/MetaServer/data/projects/labWorks/etl_Transformation_tr_salary_step2.groovy
@@ -5,8 +5,8 @@ entity.transformationSteps.find {it.name == "Projection_4"}.outputPort.fields.fi
 entity.userDefinedFunctions.clear()
 entity.mavenDependencies.clear()
 entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "labWorks").uniqueResult()
-entity.targets.find {it.name == "tableUpSalaryConflict"}.context = null
 entity.targets.find {it.name == "tableUpSalary"}.context = null
-entity.sources.find {it.name == "humanresources"}.context = session.createQuery("from etl.JdbcContext where name = :name").setParameter("name", "person").uniqueResult()
+entity.targets.find {it.name == "tableUpSalaryConflict"}.context = null
+entity.sources.find {it.name == "person"}.context = session.createQuery("from etl.JdbcContext where name = :name").setParameter("name", "person").uniqueResult()
 entity.sources.find {it.name == "hiveSalary"}.context = null
 Context.current.commit()
