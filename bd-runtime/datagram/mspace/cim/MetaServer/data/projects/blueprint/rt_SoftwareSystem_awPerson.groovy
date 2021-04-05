a0bb45dbefb7423e83db87d713796c37d6e1545b
hedaira <mezzo666@mail.ru>
2021-04-05 14:15:14 +0300

    del Connection to awHR, awPerson

--- bd-runtime/datagram/mspace/cim/MetaServer/data/projects/blueprint/rt_SoftwareSystem_awPerson.groovy
+++
@@ -1,7 +0,0 @@
-import ru.neoflex.meta.utils.Context
-def session = Context.current.txSession
-def entity = session.createQuery("from rt.SoftwareSystem where name = :name").setParameter("name", "awPerson").uniqueResult() 
-entity.project = session.createQuery("from etl.Project where name = :name").setParameter("name", "blueprint").uniqueResult()
-entity.scheme = session.createQuery("from rel.Scheme where name = :name").setParameter("name", "person_at_awPerson").uniqueResult()
-entity.defaultDeployment = session.createQuery("from rt.Deployment where name = :name").setParameter("name", "awPerson").uniqueResult()
-Context.current.commit()
